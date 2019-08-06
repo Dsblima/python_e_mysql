6526cc94f0a2f130230ffb208b088f143d89eb78
Danilo Lima <dsbl@ecomp.poli.br>
2019-08-05 23:05:17 -0300

    Estudando Comentários

--- index.py
+++ index.py
@@ -1,1 +1,21 @@
+"""
+COMENTÁRIOS
+Se houver problemas com caractéres acentuados inserir o código abaixo
+-*- coding: utf-8 -*-
+
+"""
 print("Criando o projeto")
+#COMENTARIO EM LINHA
+
+print("Testando Comentário")
+"""
+Comentario em blooo
+print('teste')
+"""
+print('após o comentario')
+
+#OPERAÇÕES MATEMÁTICAS
+
+#POTÊCIA a**b, significa a elevado a b
+
+print(2**3)

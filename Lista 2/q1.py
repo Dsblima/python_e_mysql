838509a96c4d93b8ccb90a3d6fb763f4b408d883
Danilo Lima <dsbl@ecomp.poli.br>
2019-08-13 22:38:19 -0300

    Lista de Exercício

---
+++ Lista 2/q1.py
@@ -0,0 +1,10 @@
+sequencia1 = input("Digite a sequência 1")
+sequencia2 = input("Digite a sequência 2")
+print(sequencia1)
+print('\n')
+print(sequencia2)
+print('\n')
+if sequencia1 == sequencia2:
+    print("As sequências são iguais")
+elif sequencia1 != sequencia2:
+    print("As sequencias não são iguais")
